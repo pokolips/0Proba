febd3dae8c40bc0d6e71d22f4ac9e440ca25f929
pokolips <pokolips@gmail.com>
2025-09-26 14:11:48 +0300

    Обновление 26.09

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@ void getTempAdd() {
   termo.setTemperature();     //Запрос на считывание температуры с датчиков
   thol = termo.getTempHol();  //Получение температуры холодильной камеры
   tmor = termo.getTempMor();  //Получение температуры морозильной камеры
+  tisp = termo.getTempIsp();
 }
 // *************Функция дисплея*******************************************
 void displ_Vvod() {
@@ -48,40 +49,40 @@ void text_Prob() {
   u8g.print(F(" Mor: "));
   //u8g.setPrintPos(40,0);
   u8g.print(tmor);
-  u8g.setPrintPos(88, 0);
-  u8g.print(F(" n"));
+  u8g.setPrintPos(100, 0);
+  //u8g.print(F(" n"));
   // u8g.setPrintPos(74, 0);  ---- ???????
-  //   u8g.print(tisp);
+  u8g.print(tisp);
   u8g.setPrintPos(4, 12);
   u8g.print F("Work:");
   u8g.print(wrk);  // работа устройства (камеры)F("Work:")
-                   //   //u8g.setPrintPos(56, 18);
-                   //   u8g.print(wrk); // переменная устройства
-                   //   u8g.setPrintPos(64, 12);
-                   //   u8g.print (F("Klap:")); //"Klap: "com
-                   //   // u8g.print (": "); //(knopOsv);
-                   //   u8g.print (kl); //(knopOsv);
-                      u8g.setPrintPos(0, 24);
-                      u8g.print(F("workMor:"));
-                      u8g.print(workMor);
-                   //   u8g.setPrintPos(68, 24);
-                   //   u8g.print (F("imp: "));
-                   // // myString s;
-                   //   u8g.print(imp);
+  u8g.setPrintPos(56, 18);
+  //   u8g.print(wrk); // переменная устройства
+  //   u8g.setPrintPos(64, 12);
+  u8g.print(F("Compr:"));  //"Klap: "com
+                           //   // u8g.print (": "); //(knopOsv);
+  u8g.print(s);            //(knopOsv);
+  u8g.setPrintPos(0, 24);
+  u8g.print(F("workMor:"));
+  u8g.print(workMor);
+  //   u8g.setPrintPos(68, 24);
+  //   u8g.print (F("imp: "));
+  // // myString s;
+  //   u8g.print(imp);
   u8g.setPrintPos(0, 36);
   u8g.print(F("time: "));
   //   u8g.print(s4);//"time: "
   u8g.print(s1);
   //   //u8g.print(isWorkKlap); //s4 "/ "
   //   u8g.print(emagn);
-  //   u8g.setPrintPos(68, 36);
-  //   u8g.print(stm);
-  //   u8g.print(summHol);
-  //   u8g.print(F(":"));
-  //   u8g.print(summMor);
-  //   u8g.setPrintPos(0, 48);
-  //   u8g.print(F("SummIs:"));
-  //   u8g.print(isSummIsp);
+  u8g.setPrintPos(68, 36);
+  u8g.print(com);  //   u8g.print(stm);
+                   //   u8g.print(summHol);
+                   //   u8g.print(F(":"));
+                   //   u8g.print(summMor);
+  u8g.setPrintPos(0, 48);
+  u8g.print(F("SummMor:"));
+  u8g.print(isComprOn);
   //   u8g.setPrintPos(68, 48);
   //   u8g.print(F("New:"));
   //     u8g.print(vs);
@@ -127,19 +128,19 @@ void setup() {
     u8g.setPrintPos(8, 35);
     u8g.print(F(" _0Proba "));  //Написать download?
     u8g.setPrintPos(15, 45);
-    u8g.print(F("26.09.2025 "));
+    u8g.print(F("09.09.2025 "));
   } while (u8g.nextPage());  //Заканчивает вывод на экран
 
   delay(30000);  //900000; 15 минут
   prKn == 1;
   comprEndCycle();  //Функция задержки
-  getTempAdd();     //Получение температуры
-  //proverkaKn();// Функция проверки положения клапана
-  //if (prKn == 1) {
-    proverkaKn();  // Функция проверки положения клапана
-    prKn == 0;
-    
-    isComprOn == 0;
+                    //getTempAdd();     //Получение температуры
+                    //proverkaKn();// Функция проверки положения клапана
+                    //if (prKn == 1) {
+  proverkaKn();     // Функция проверки положения клапана
+  prKn == 0;
+
+  //isComprOn == 0;
   //}
   attachInterrupt(0, isr, RISING);
   attachInterrupt(1, awr, RISING);
@@ -172,38 +173,60 @@ void awr() {
 //##################### Основной цикл  #####################
 void loop() {
   /*Проверяет температуру, переменную отключения компрессора, состояние клапана.*/
-  uint8_t tmpr = 0;
-  bool error;
-
-    if (isComprOn == 0 && !flag) {
+  // uint8_t tmpr = 0;
+  // bool error;
+  unsigned long currentMillis = millis();
+  com = (F("loop"));
+  if (isComprOn == 1 && flag == false) {
     digitalWrite(compr, HIGH);
-  } else if (isComprOn == 1 || flag) {
+    flag = true;  // и тут ошибка ==
+    s = (F("Work !"));
+    //Сюда написать код с задержкой 5 минут для работы
+  } else if (isComprOn == 0 && flag == true) {
     digitalWrite(compr, LOW);
+    s = (F("isComprOn !"));
     comprEndCycle();
   }
-
+  // -------------------------------------
   if (millis() - myTimer1 >= 30000) {
     myTimer1 = millis();  // сбросить таймер
     getTempAdd();
-    isComprOn = cycl.goCompr(thol, tmor);
+    //isComprOn = cycl.goCompr(thol, tmor);// временно убрал для проверки
     kn = sklad.getKn();
     delay(5);
   }
-
-  if (compr == HIGH) {
-    if (millis() - myTimer1 >= 1200000) {
-      myTimer1 = millis();  // сбросить таймер
-      flag = true;// Флаг подняли
-      comprEndCycle();  
+  // -------------------------------------
+  if (digitalRead(compr) == HIGH) {  // Ещё одна ошибка
+    s = (F("ON"));                   //Повторения не будет?----------
+    if (millis() - myTimer4 >= 2400000) {
+      myTimer4 = millis();  // сбросить таймер
+      //flag = true;                          // Флаг подняли
+      stch = 0;
+      isComprOn = 0;  //ошибка! было ==
+      //comprEndCycle();
       delay(5);
     }
+    // -------------------------------------
+    if (!isLoadOn && currentMillis - previousMillis >= intervalOff) {
+      // Включаем нагрузку после интервала выключения
+      isLoadOn = true;
+      previousMillis = currentMillis;
+      digitalWrite(fanPin, HIGH);
+    } else if (isLoadOn && currentMillis - previousMillis >= intervalOn) {
+      // Выключаем нагрузку после интервала включения
+      isLoadOn = false;
+      previousMillis = currentMillis;
+      digitalWrite(fanPin, LOW);
+    }
   }
+  // -------------------------------------
   if (isCheckingPass) {
     getTempAdd();                  // Проверяем тем-ру
     termo.setTempOld(thol, tmor);  // Записываем в память
       // считанную температуру передаём в класс getOldTemp();
     isCheckingPass = !isCheckingPass;
   }
+  // -------------------------------------
   if (millis() - myTimer2 >= 30000) {
     myTimer2 = millis();  // сбросить таймер
     uint8_t temOld = termo.getOldTemp();
@@ -230,28 +253,27 @@ void loop() {
     delay(5);
   }
   //проверяем: если кн == фолсе тогда переменная уст. в фалсе иначе если кн == труе переменная уст. труе.
-  if (klapWork && flag) { klapan; }
-      Serial.println(thol);
-      Serial.println(tmor);
+  // if (klapWork && flag) { klapan; }
+  //     Serial.println(thol);
+  //     Serial.println(tmor);
 
-  //-------- Вывод на дисплей -------------     
- if (millis() - myTimer3 >= 60000) {
+  //-------- Вывод на дисплей -------------
+  if (millis() - myTimer3 >= 60000) {
     myTimer3 = millis();  // сбросить таймер
-    getTempAdd();// Получаем температуру
-    if(digitalRead(5) == HIGH){
+    getTempAdd();         // Получаем температуру
+    if (digitalRead(5) == HIGH) {
       wrk = (F("GO "));
-    }else if(digitalRead(5) == LOW){
+    } else if (digitalRead(5) == LOW) {
       wrk = (F("STOP "));
     }
     stch++;
     s1 = stch;
-    displ_Vvod();//выводим на дисплей
+    displ_Vvod();  //выводим на дисплей
     delay(5);
   }
 
   delay(100);
 }
-
 //###################################
 void comprEndCycle() {
   // Сюда поставить таймер из гайвера ? Зачем?
@@ -259,13 +281,9 @@ void comprEndCycle() {
     digitalWrite(compr, LOW);
     s = (F("OFF"));
   }
-  com = (F("Compr"));
+  com = (F("ComEnd"));
   // kl = "PAUSE";
   wrk = (F("PAUSE"));
-  // imp = isWorkKlap;
-  s4 = (F("EndCy"));
-  s1 = (F("P"));
-  provH = (F("cEndC"));
   int i = 15;
 
   unsigned long comprTimeStamp = millis();  //Время включения компрессора
@@ -289,25 +307,36 @@ void proverkaKn() {
     digitalWrite(compr, HIGH);
     s = (F("ON"));
   }
+  com = (F("Pr-ka"));
   wrk = (F("PROVERKA "));
   uint8_t stchet = 0;
+  getTempAdd();
   int8_t thl = thol;
   int8_t tmr = tmor;
   kn = sklad.getKn();
   bool klpn = true;
   int i = 0;
-  while (klpn = true) {
+  while (klpn == true && i <= 80) {
     stchet++;
     getTempAdd();
     if ((thol + 2) <= thl) {
       kn = true;
       klpn = false;
+      com = (F("thol"));
     } else if ((tmor + 2) <= tmr) {
       kn = false;
       klpn = false;
-    }
+      com = (F("tmor"));
+    } else (F("net "));
     i++;
     s1 = i;
+    isSummIsp = (tmor + 2) - tmr;  // было неправильно
+    workMor = klpn;
+    // if(i == 18) {digitalWrite(7, HIGH);
+    // delay(30000);
+    // digitalWrite(7,
+    // LOW);
+    // }
     displ_Vvod();
     delay(30000);
   }
@@ -317,7 +346,6 @@ void proverkaKn() {
     prKn = 1;
   } else prKn = 0;
   // if(prKn == 1 && i <= 15){prKn = 1// нужна проверка клапана}
-
   //     }
   sklad.setKn(kn);
   comprEndCycle();
